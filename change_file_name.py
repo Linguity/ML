c3ab6a76c0b8b17ba77de56da562254c3f941a38
pradanaadn <pradanaadn21@gmail.com>
2023-06-09 21:07:42 +0800

    Revert "SPLIT TEST AND TRAIN 6:4"
    This reverts commit ec40e1e2a89b17c2f34f1d902b90fc571a6fe421.

--- change_file_name.py
+++ change_file_name.py
@@ -1,13 +1,14 @@
 import os
 
 def change_name(root_path):
-    split_path = os.path.join(root_path,"test")
+    split_path = os.path.join(root_path,"TEST")
     for folders in os.listdir(split_path):
         folders_path = os.path.join(split_path,folders)
         for index, files in enumerate(os.listdir(folders_path)):
                 old_name = os.path.join(folders_path,files)
                 new_name = os.path.join(folders_path,"TEST_{}_{}.wav".format(folders,index))
                 os.rename(old_name,new_name)
+                # print(old_name)
                 # if os.path.exists(new_name):
                 #     os.remove(new_name)
 if __name__ == "__main__":
